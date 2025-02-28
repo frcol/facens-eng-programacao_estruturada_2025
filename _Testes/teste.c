28fad84873e540e287ffc6a003276aa4b3720046
Fabio Colombini <fabiorcolombini@gmail.com>
2025-02-28 19:07:15 -0300

    Aula 3

--- _Testes/teste.c
+++ _Testes/teste.c
@@ -2,18 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-float converter(float valorFahrenheit);
+// Prototipo
+int soma(int num1, int num2);
 
 main() {
     system("cls");
     // ===============================================
-    
-    int array[10] = {1,2,3,4,5,6,7,8,9,10};
+    int resultado = soma(5, 3);
 
-    printf("%i\n", array[3]);
-    array[3] = 1000;
-    printf("%i", array[3]);
+    printf("%i", resultado);
 
     // ===============================================
     printf("\n\n");
+}
+
+int soma(int num1, int num2) {
+    int result = num1+num2;
+
+    if (result > 10) {
+        result = 1;
+    } 
+    else {
+        result = 0;
+    }
+
+    return result;
 }
