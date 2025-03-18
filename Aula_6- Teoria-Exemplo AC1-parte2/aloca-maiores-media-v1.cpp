f3b773b81d93b3a809948309e44e330fb6942054
Fabio Colombini <fabiorcolombini@gmail.com>
2025-03-17 22:28:33 -0300

    aula 6

---
+++ Aula_6- Teoria-Exemplo AC1-parte2/aloca-maiores-media-v1.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void aloca(float **p, int tam);
+void leitura(float *p);
+int media(float *p, int tam, float **pM);
+void mostra(float *p, int tam);
+
+main()
+{
+  float *ptr = NULL, *pmaior = NULL;
+  char op;
+  int cont = 0, qmaior;
+
+  do
+  {
+    aloca(&ptr, cont + 1);
+    leitura(ptr + cont);
+    cont++;
+
+    printf("\nDeseja continuar <S/N>: ");
+    scanf("%c", &op);
+    fflush(stdin);
+  } while (op != 'n' && op != 'N');
+
+  printf("\nNumeros digitados:\n");
+  mostra(ptr, cont);
+  qmaior = media(ptr, cont, &pmaior);
+
+  printf("\n\nMaiores que a media\n");
+  mostra(pmaior, qmaior);
+
+  printf("\n\n");
+}
+
+void aloca(float **p, int tam)
+{
+  if ((*p = (float *)realloc(*p, tam * sizeof(float))) == NULL)
+    exit(1);
+}
+
+void leitura(float *p)
+{
+  printf("\nNumero: ");
+  scanf("%f", p);
+  fflush(stdin);
+}
+
+int media(float *p, int tam, float **pM)
+{
+  int i, qM = 0;
+  float m = 0;
+
+  for (i = 0; i < tam; i++, p++)
+    m += *p;
+
+  m /= tam;
+
+  printf("\nMedia = %.2f\n\n", m);
+
+  p -= tam; // tem que retornar o ponteiro p para o in�cio
+  
+  for (i = 0; i < tam; i++, p++)
+    if (*p > m) // maior
+    {
+      aloca(pM, qM + 1);
+      *(*pM + qM) = *p;
+      qM++;
+    }
+  return qM;
+}
+
+void mostra(float *p, int tam)
+{
+  int i;
+  for (i = 0; i < tam; i++)
+    printf("\n%.2f", *(p + i));
+}
---
+++ Aula_6- Teoria-Exemplo AC1-parte2/prova-AC1/EXTRA_aloca-subtracao-pares.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void aloca(int **p,int tam);
+void leitura(int *p);
+int pares(int *p,int tam,int **pp);
+void mostra(int *p,int tam);
+
+main()
+{
+int *ptr=NULL,cont=0,*ppar=NULL,qpar;
+char op;
+do{
+  aloca(&ptr,cont+1);
+  leitura(ptr+cont);
+  cont++;
+  printf("\nDeseja continuar <S/N>: ");
+  scanf("%c",&op);
+  fflush(stdin);
+  }while(op!='n' && op!='N');
+printf("\nNumeros digitados:\n");
+mostra(ptr,cont);
+qpar=pares(ptr,cont,&ppar);
+printf("\n\nSubtracao - Par(es):\n");
+mostra(ppar,qpar);
+printf("\n\n");
+system("pause");
+}//main
+  
+void aloca(int **p,int tam)
+{
+if((*p=(int*)realloc(*p,tam*sizeof(int)))==NULL)
+  exit(1);
+}// aloca
+
+void leitura(int *p)
+{  
+printf("\nNumero: ");
+scanf("%i",p);
+fflush(stdin);
+}//leitura
+
+int pares(int *p,int tam,int **pp)
+{
+int i,sub,qp=0;
+for(i=0;i<tam/2;i++)
+  {
+  sub=*(p+i)-(*(p+tam-1-i));
+  if(sub%2==0)  //par
+    { 
+    aloca(pp,qp+1);
+    *(*pp+qp)=sub;
+    qp++;
+    }//if
+  }//for
+if(tam%2==1)    //qtde for impar
+  if(*(p+(tam/2))%2==0)   //elemento impar = par
+    { 
+    aloca(pp,qp+1);
+    *(*pp+qp)=*(p+(tam/2));
+    qp++;
+    }//if
+return qp;
+}//pares
+    
+void mostra(int *p,int tam)
+{
+int i;
+for(i=0;i<tam;i++)
+  printf("\n%i",*(p+i));
+}//mostra
